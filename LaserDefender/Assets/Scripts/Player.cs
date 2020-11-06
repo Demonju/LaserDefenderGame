d0c28908161110a02f25f982e7c938dba91660f3
Demonju <jake.zammit.d57298@mcast.edu.mt>
2020-11-06 13:41:11 +0100

    Updated movement

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 10f;
+
+    float xMin, xMax, yMin, yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
@@ -17,20 +22,35 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    //sets up the boundaries according to the camera
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        //xMin = 0 according to Camera view
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 0.5f, 0)).y;
+    }
+
     //moves the Player ship
     private void Move()
     {
         //var changes its variable type
         //depending on what i save it in
         //deltaX will have the difference in the x-axis which the player moves
-        var deltaX = Input.GetAxis("Horizontal");
-
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         //newXPos = current x-position     + difference in x
         var newXPos = transform.position.x + deltaX;
+        //clamp the ship between xMin and xMax
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
 
         //the above in y axis:
-        var deltaY = Input.GetAxis("Vertical");
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
         var newYPos = transform.position.y + deltaY;
+        //clamp the ship between yMin and yMax
+        newYPos = Mathf.Clamp(newYPos, yMin, yMax);
 
         //move the Player ship to the newXPos
         this.transform.position = new Vector2(newXPos, newYPos);
