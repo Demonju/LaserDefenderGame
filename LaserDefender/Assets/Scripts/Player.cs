fd4f811b27e10adf198bdc1ef79ffe49f6129f67
Demonju <jake.zammit.d57298@mcast.edu.mt>
2020-11-06 01:05:23 +0100

    Movement
    moving player

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +14,25 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    //moves the Player ship
+    private void Move()
+    {
+        //var changes its variable type
+        //depending on what i save it in
+        //deltaX will have the difference in the x-axis which the player moves
+        var deltaX = Input.GetAxis("Horizontal");
+
+        //newXPos = current x-position     + difference in x
+        var newXPos = transform.position.x + deltaX;
+
+        //the above in y axis:
+        var deltaY = Input.GetAxis("Vertical");
+        var newYPos = transform.position.y + deltaY;
+
+        //move the Player ship to the newXPos
+        this.transform.position = new Vector2(newXPos, newYPos);
     }
 }
