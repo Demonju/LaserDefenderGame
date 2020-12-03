41361b6bec856555e465cf73062a71ad24d0a44a
Demonju <jake.zammit.d57298@mcast.edu.mt>
2020-12-03 21:03:12 +0100

    Health + Damage Added

---
+++ LaserDefender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float health = 500f;
+
+    [SerializeField] float shotCounter;
+
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    [SerializeField] GameObject enemyLaserPrefab;
+
+    [SerializeField] float enemyLaserSpeed = 0.3f;
+
+    //reduces health whenever the enemy collides with a gameObject
+    //which has a DamageDealer component
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        //access the DamageDealer class from "otherObject" which hits enemy
+        //and reduces health accordingly
+        DamageDealer dmgDealer = otherObject.gameObject.GetComponent<DamageDealer>();
+
+        ProcessHit(dmgDealer);
+    }
+
+    //Whenever ProcessHit() is called, send us the DamageDealer details
+    private void ProcessHit(DamageDealer dmgDealer)
+    {
+        health -= dmgDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //generate a random number
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        // every frame reduce the amount of time for shot
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0f)
+        {
+            EnemyFire();
+            //reset ShotCounter
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void EnemyFire()
+    {
+        //spawn an enemy laser and Enemy Position
+        GameObject enemyLaser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+        //shoot laser downwards: -enemyLaserSpeed
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyLaserSpeed);
+    }
+}
